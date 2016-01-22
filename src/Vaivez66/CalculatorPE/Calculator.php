80e744fdd8313f85f95fd4e7120855fa3d4d22af
Reziq Vavai <kakakvavai66@gmail.com>
2016-01-22 14:04:10 +0700

    Update Calculator.php

--- src/Vaivez66/CalculatorPE/Calculator.php
+++ src/Vaivez66/CalculatorPE/Calculator.php
@@ -13,47 +13,48 @@ use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat as TF;
 
 class Calculator extends PluginBase implements Listener{
-
+    
     public function onEnable(){
-		$this->getServer()->getLogger()->info(TF::GREEN . "CalculatorPE is ready!");
-		$this->getCommand("cal")->setExecutor(new CalculatorCommand($this));
-	}
+    	$this->getServer()->getLogger()->info(TF::GREEN . "CalculatorPE is ready!");
+    	$this->getCommand("cal")->setExecutor(new CalculatorCommand($this));
+    }
+    
+    public function add(array $num){
+    	$result = 0;
+    	foreach($num as $n){
+    	    $result += $n;
+    	}
+    	return $result;
+    }
 
-	public function add(array $num){
-		$result = 0;
-		foreach($num as $n){
-			$result += $n;
-		}
-		return $result;
+    public function sub(array $num){
+    	$result = $num[0];
+    	array_shift($num);
+    	foreach($num as $n){
+    	    $result -= $n;
+    	}
+    	return $result;
+    }
+    
+    public function multiply(array $num){
+	$result = 1;
+	foreach($num as $n){
+	    $result *= $n;
 	}
+	return $result;
+    }
+    
+    public function divide(array $num){
+	$result = $num[0];
+	array_shift($num);
+	foreach($num as $n){
+	    $result /= $n;
+	}
+	return $result;
+    }
 
-	public function sub(array $num){
-		$result = $num[0];
-		array_shift($num);
-		foreach($num as $n){
-			$result -= $n;
-		}
-		return $result;
-	}
-
-	public function multiply(array $num){
-		$result = 1;
-		foreach($num as $n){
-			$result *= $n;
-		}
-		return $result;
-	}
-
-	public function divide(array $num){
-		$result = $num[0];
-		array_shift($num);
-		foreach($num as $n){
-			$result /= $n;
-		}
-		return $result;
-	}
-
-	public function onDisable(){
-		$this->getServer()->getLogger()->info(TF::RED . "CalculatorPE was disabled!");
-	}
+    public function onDisable(){
+	$this->getServer()->getLogger()->info(TF::RED . "CalculatorPE was disabled!");
+    }
+    
 }
